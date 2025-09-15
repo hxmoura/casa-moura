0cb4b8b3701a38b54df7b2b50d9c77a29669241d
Henrique Moura <hxcmoura@outlook.com>
2025-09-15 15:18:58 -0300

    fix: adjust integration with Mercado Pago

--- src/app/api/mp/route.ts
+++ src/app/api/mp/route.ts
@@ -1,6 +1,6 @@
 import MercadoPagoConfig, { Payment } from "mercadopago";
-import { Response } from "../../../api/services/Response";
 import { NextResponse } from "next/server";
+import { Response } from "../../../api/services/Response";
 
 const client = new MercadoPagoConfig({
   accessToken: process.env.MP_SERVER_KEY || "",
@@ -11,30 +11,38 @@ export async function POST(req: Request) {
     const order = await req.json();
     const payment = new Payment(client);
 
-    const pay = await payment.create({
-      body: {
-        token: order.token,
-        transaction_amount: order.price,
-        payment_method_id: order.paymentMethod,
-        installments: order.installments,
-        payer: {
-          email: order.user.email,
-          first_name: order.user.name,
-          last_name: order.user.lastname,
-          identification: {
-            type: "CPF",
-            number: order.user.cpf,
-          },
-          address: {
-            zip_code: order.user.delivery.cep,
-            city: order.user.delivery.city,
-            neighborhood: order.user.delivery.neighborhood,
-            street_name: order.user.delivery.address,
-            street_number: order.user.delivery.number,
-            federal_unit: order.user.delivery.state,
-          },
+    const body: any = {
+      transaction_amount: Number(order.price),
+      payment_method_id: order.paymentMethod,
+      installments: order.installments || 1,
+      payer: {
+        email: order.user.email,
+        first_name: order.user.name,
+        last_name: order.user.lastname,
+        identification: {
+          type: "CPF",
+          number: order.user.cpf,
+        },
+        address: {
+          zip_code: order.user.delivery.cep,
+          city: order.user.delivery.city,
+          neighborhood: order.user.delivery.neighborhood,
+          street_name: order.user.delivery.address,
+          street_number: order.user.delivery.number,
+          federal_unit: order.user.delivery.state,
         },
       },
+    };
+
+    if (
+      order.paymentMethod !== "pix" &&
+      order.paymentMethod !== "bolbradesco"
+    ) {
+      body.token = order.token;
+    }
+
+    const pay = await payment.create({
+      body,
       requestOptions: { idempotencyKey: String(Date.now()) },
     });
 
